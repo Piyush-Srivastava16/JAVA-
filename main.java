d804aa5f6f2625e634ef9c7b50a88116562106ef
PIYUSH <PIYUSHSRIVASTAVA7275@GMAIL.COM>
2026-01-19 21:40:03 +0530

    GH

---
+++ main.java
@@ -0,0 +1,164 @@
+import java.util.*;
+
+public class main {
+    // public static void main (String[] args) {
+    // Scanner sc =new Scanner(System.in);
+    // System.out.println("ENTER TYHE FIRST NUMBER ");
+    // int a = sc.nextInt();
+    // System.out.println("ENETER THE SECOND NUMBER");
+    // int b= sc.nextInt();
+    // int sum = 2*(a+b);
+    // System.out.println("THE ADDITION OF THIS NUMBER IS "+sum);
+    // Scanner st = new Scanner(System.in);
+    // String string =st.nextLine();
+    // System.out.println("THIS IS THE STRING NAME "+string);
+    // Scanner nxt = new Scanner(System.in);
+    // String next = nxt.next();
+    // System.out.println("THIS THIS THE ONLY NEXT STRING "+next);
+
+    //
+    // ------------------------------------------------------------------------------------------------------------
+    // WITH VOID
+    // CALL-------------------------------------------------------------------------------------
+    // public static void sum(int a,int b) {
+    // int sum = a +b;
+    // System.out.println(sum);
+    // }
+    // public static void main(String[] args) {
+    // Scanner sc =new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // sum(a,b);
+    // }
+
+    // ------------------------------------------------------------------------------------------------------------------
+    // int call ------------------------------------------------------------------
+    // public static int sum (int a,int b) {
+    // int sum = a*b;
+    // return sum;
+    // }
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("ENTER THE FIRST NUMBER");
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int store_call =sum(a,b);
+    // System.out.println( store_call );
+    // }
+
+    // -------------------------------------------------------------------------------------------------------------------
+    // FOR LOOP
+    // -----------------------------------------------------------------------------------------
+
+    // public static void main(String[] args) {
+    // for(int i=0 ;i<=5 ; i++){
+    // for(int j=i ; j<=i ; j++)
+    // System.out.println("*");
+    // }
+
+    // }
+    // public static void hollow_rectangle() {
+    //     for (int i = 1; i <= 4; i++) {
+    //         for (int j = 1; j <= 5; j++) {
+    //             if (i == 1 || i == 4 || j == 1 || j == 5) {
+    //                 System.out.print("*");
+    //             } else {
+    //                 System.out.print(" ");
+    //             }
+
+    //         }
+    //         System.out.println("a");
+
+    //     }
+    // }
+
+//     public static void star(){
+//         for (int i=0;i<=5;i++){
+//                 for (int j=0;j<=i;j++){
+//                      System.out.print("*");
+//                 }
+//                         System.out.println(" ");
+//         }
+
+//     }
+    
+
+//         public static void stars(){
+//         for (int i=0;i<=5;i++){
+//                 for (int j=0;j<=i;j++){
+//                      System.out.print("*");
+//                 }
+//                         System.out.println(" ");
+//         }
+
+//     }
+//     public static void inverted_triangle(){
+//         for (int i=1;i<+5;i++){
+//             for(int j=1;j<=5-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println(" ");
+//         }
+//     }
+//     public static void hollow (){
+//         for (int i=1;i<=5;i++){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print(" ");
+//             }
+//             System.out.println(" ");
+//         }
+//     }
+    
+//     public static void main (String[] args) {
+//         hollow_rectangle();
+//         System.out.println("");
+//         star();
+//         stars();
+//         inverted_triangle();
+//         hollow();
+//     }
+
+// }
+// public static void inverted(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<5-i+1;j++){
+//             System.out.print(j);
+//         }
+//     }
+//     System.out.println("");
+// }
+// public static void main (String[] args) {
+//     inverted();
+//
+// public static void inverted_half_pyramid(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i+1;j++){
+//            // System.out.print(j+" ");
+//            // System.out.print("\n");
+//           System.out.print(j);
+//         }
+//          System.out.println("");
+       
+//     }
+    
+// }
+ public static void floyd(){
+    int counter=1;
+    for(int i=1;i<=5;i++){
+        for(int j=i;j<=i+1;j++){
+
+            System.out.print(j);
+        }
+        System.err.println("");
+    }
+}
+public static void main(String[] args) {
+    //inverted_half_pyramid(5);
+    floyd();
+}
+}
+
+
